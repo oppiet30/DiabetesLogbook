a2a9cded9e2797deb401dde3e61b2e14e183ef44
Robert Hinson <oppiet35@yahoo.com>
2024-08-18 13:14:59 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Diabetes Logbook</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="w3.css">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+    body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+    .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+    .fa-anchor,.fa-coffee {font-size:200px}
+    /*table, th, td {border: 1px solid black; border-collapse: collapse;}*/
+</style>
+
+<!-- Navbar -->
+<div class="w3-top">
+    <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+        <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+        <a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+        <a href="previous_logs.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Previous Logs</a>
+        <a href="add_food.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Add Food</a>
+    </div>
+
+    <!-- Navbar on small screens -->
+    <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Add Meal</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Previous Logs</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
+    </div>
+</div>
