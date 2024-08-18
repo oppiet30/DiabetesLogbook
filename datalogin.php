1f981079abc1b880106c1c88440d8bb3044e0538
Robert Hinson <oppiet35@yahoo.com>
2024-08-18 13:08:04 -0400

    Create datalogin.php

---
+++ datalogin.php
@@ -0,0 +1,10 @@
+<?php
+// Basic connection settings
+$databaseHost = '';
+$databaseUsername = '';
+$databasePassword = '';
+$databaseName = 'diabeteslogbook';
+
+// Connect to the database
+$db = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName)
+?>
