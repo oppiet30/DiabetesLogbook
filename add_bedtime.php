3fc1b064bba76fccaf27574d8cff6d35ba5edb48
Robert Hinson <oppiet35@yahoo.com>
2024-08-18 13:03:42 -0400

    Create add_bedtime.php

---
+++ add_bedtime.php
@@ -0,0 +1,38 @@
+<?php
+include "server.php";
+$value = $_GET['column'];
+$date = $_GET['date'];
+include "header.php";
+?>
+
+<html>
+<body>
+<div class="w3-row-padding w3-padding-64 w3-container">
+    <div class="w3-content">
+        <div class="w3-twothird">
+            <h1><?php echo $value; ?></h1>
+            <form method="post" action="<?php echo $_SERVER['HTTP_REFERER']; ?>">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Blood Sugar</th>
+                            <td>
+                                <input type="text" name="blood_sugar" required>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <input type="hidden" name="entry_type" value="<?php echo $value; ?>">
+                                <input type="hidden" name="date" value="<?php echo $date; ?>">
+                                <button type="submit" class="btn" name="add_bedtime">Submit</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
